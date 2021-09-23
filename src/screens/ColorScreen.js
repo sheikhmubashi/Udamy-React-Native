483c32d478cab46cb245f9d8eabba144a3b86d21
Sheikh Mubashir <smubashir655@gmail.com>
2021-09-23 17:24:34 +0500

    Update ColorScreen.js

--- src/screens/ColorScreen.js
+++ src/screens/ColorScreen.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { View, StyleSheet, Button } from 'react-native';
 
 const ColorScreen = () => {
+    const [colors, setColors] = useState([]);
     return <View>
-        <Button title='Add a Color'/>
+        <Button 
+        title='Add a Color'
+        onPress={()=>{setColors([...colors, rendomRgb()])}}
+        />
         <View style={{width: 100, height: 100, backgroundColor: rendomRgb()}}></View>
     </View>
 }
