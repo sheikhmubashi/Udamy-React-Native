052a2b6ad9adae5380b24b212e44201792049ecb
Sheikh Mubashir <smubashir655@gmail.com>
2021-09-24 16:36:15 +0500

    ds

--- src/screens/SquireScreen.js
+++ src/screens/SquireScreen.js
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import ColorCounter from '../Components/ColorCounter';
 
 const SquireScreen = () => {
+    const [red, setRed] = useState(0);
+    const [green, setGreen] = useState(0);
+    const [blue, setBlue] = useState(0);
+
+
     return <View>
-        <ColorCounter color='red'/>
-        <ColorCounter color='green'/>
-        <ColorCounter color='blue'/>
+        <ColorCounter
+            onIncrease={() => setRed(red + 1)}
+            onDecrease={() => setRed(red - 1)}
+            color='red' />
+        <ColorCounter color='green' />
+        <ColorCounter color='blue' />
 
     </View>
 
