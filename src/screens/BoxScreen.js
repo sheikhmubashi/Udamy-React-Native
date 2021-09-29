417f621e2b3e938c54821d96152baaaff95622f8
Sheikh Mubashir <smubashir655@gmail.com>
2021-09-29 16:47:07 +0500

    Update BoxScreen.js

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -2,11 +2,29 @@ import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
 const BoxScreen = () => {
-    return <View>
-        <Text>Box Screen</Text>
+    return <View style={styles.parentStyle}>
+        <View style={viewOneStyle}></View>
+        <View style={viewTwoStyle}></View>
+        <View style={viewThreeStyle}></View>
     </View>
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    parentStyle: {
+        borderWidth: 3,
+        borderColor: 'black',
+        height: 200
+    },
+    viewOneStyle: {
+        height:50,
+        width: 50,
+    },
+    viewTwoStyle: {
+
+    },
+    viewThreeStyle: {
+
+    }
+})
 
 export default BoxScreen;
